8f43e4a2f0585fd0bb2adba34ff0879700f05aef
curryfizz <nawsheen141@gmail.com>
2023-02-10 03:11:08 +0600

    mongodb connection doesnt work yet

--- config/db.js
+++ config/db.js
@@ -1,5 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async() => {
-    await mongoose.connect()
-}
+    await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        // useCreateIndex: true,
+        useUnifiedTopology: true
+        // useFindAndModify: true
+    });
+
+    console.log("MongoDB connected");
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 require('dotenv').config({path:"./config.env"});
 const express = require("express");
+const connectDB = require("./config/db");
+
+//connect db
+connectDB();
 
 const app = express();
 
@@ -8,5 +12,9 @@ app.use("/api/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`));
+const server = app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`));
 
+process.on("unhandledRejection", (err, promise)=>{
+    console.log(`Logged Error: ${err}`);
+    server.close(() => process.exit(1));
+})
