52428eff0d896282bdd6893188c63fd1070a11a3
curryfizz <nawsheen141@gmail.com>
2023-02-10 19:48:42 +0600

    mongodb(atlas) connection doneeeeee

--- config/db.js
+++ config/db.js
@@ -2,13 +2,16 @@ const mongoose = require("mongoose");
 
 const connectDB = async() => {
     await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        // useCreateIndex: true,
-        useUnifiedTopology: true
-        // useFindAndModify: true
+    
+    useNewUrlParser: true, 
+
+    useUnifiedTopology: true 
+
+    }, err => {
+    if(err) throw err;
+    console.log('Connected to MongoDB!!!')
     });
 
-    console.log("MongoDB connected");
 };
 
 module.exports = connectDB;
