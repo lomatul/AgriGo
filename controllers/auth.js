f78d7b0eb2f53ca17e3bcfd8be5b678d5fbbd720
curryfizz <nawsheen141@gmail.com>
2023-02-13 02:31:48 +0600

    created custom error handlers, added tokens  and refactored functions

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const ErrorResponse = require("../utils/errorResponse");
 
 exports.register = async (req, res, next) => {
     const{username, email, password} = req.body;
@@ -7,42 +8,45 @@ exports.register = async (req, res, next) => {
         const user = await User.create({
             username, email, password,
         });
-        res.status(201).json({
-            success:true,
-            user,
-        });
+        // res.status(201).json({
+        //     success:true,
+        //     token: "234324",
+        // });
+
+        sendToken(user, 201, res);
     }catch(error){
-        res.status(500).json({
-            success: false,
-            error: error.message,
-        });
+        //replacing with new errorhandler stuff
+        next(error);
+        // res.status(500).json({
+        //     success: false,
+        //     error: error.message,
+        // });
     }
 };
 
 exports.login = async (req, res, next) => {
     const {email, password} = req.body;
 
-    if(!email||!password){
-        res.status(400).json({success: false, error: "Please provide email and password"})
+    if (!email || !password) {
+        return next(new ErrorResponse("Please provide an email and password", 400));
     }
 
-    try{
-        const user = await User.findOne({email}).select("+password");
+    try {
+    // Check that user exists by email
+    const user = await User.findOne({ email }).select("+password");
 
-        if(!user){
-            res.status(404).json({success: false, error:"Invalid credentials"})
-        }
+    if (!user) {
+        return next(new ErrorResponse("Invalid credentials", 401));
+    }
 
-        const isMatch = await user.matchPassword(password);
+    // Check that password match
+    const isMatch = await user.matchPassword(password);
 
-        if(!isMatch){
-            res.status(404).json({success:false,error:"Invalid credentials"})
-        }
+    if (!isMatch) {
+        return next(new ErrorResponse("Invalid credentials", 401));
+    }
 
-        res.status(200).json({
-            success: true,
-            token: "rwerwerewc22",
-        });
+    sendToken(user, 200, res);
     }catch(error){
         res.status(500).json({success:false, error: error.message});
     }
@@ -59,3 +63,7 @@ exports.resetpassword = (req, res, next) => {
 };
 
 
+const sendToken = (user, statusCode,res)=>{
+    const token = user.getSignedToken();
+    res.status(statusCode).json({success:true,token})
+};
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 require('dotenv').config({path:"./config.env"});
 const express = require("express");
 const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
 
-//conect db
+//connect db
 connectDB();
 
 const app = express();
@@ -10,6 +11,9 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth"));
+
+//Error handler (should be last piece of middleare)
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
