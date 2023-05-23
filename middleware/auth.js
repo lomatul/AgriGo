f89547009a5e383b7805820c26e8bca295260099
Lomatul Mahzabin <90206489+lomatul@users.noreply.github.com>
2023-05-23 21:07:10 +0600

    Add files via upload

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,31 +3,39 @@ const User = require("../models/User");
 const ErrorResponse = require("../utils/errorResponse");
 
 
-exports.protect = async(req,res,next)=>{
+exports.protect = async (req, res, next) => {
     let token;
-    if(req.headers.authorization &&
-        req.headers.authorization.startsWith("Bearer")){
-        token = req.headers.authorization.split(" ")[1];
-
+  
+    // Check if the token is present in the "Authorization" header
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+      token = req.headers.authorization.split(" ")[1];
     }
-
-    if(!token){
-        return next(new ErrorResponse("Not authorized to access this route", 401));
+  
+    // If token not found in the header, check the "accessToken" cookie
+    if (!token && req.cookies.accessToken) {
+      token = req.cookies.accessToken;
     }
-
-    try{
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        
-        const user = await User.findById(decoded.id);
-
-        if(!user){
-            return next(new ErrorResponse("No user found with this id", 404));
-        }
-
-        req.user = user;
-
-        next();
-    }catch(error){
-        return next(new ErrorResponse("Not authorized to access this route", 401));
+  
+    if (!token) {
+      // No token was found
+      return next(new ErrorResponse("Not authorized to access this route, no token", 401));
     }
-};
+  
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  
+      const user = await User.findById(decoded.id);
+  
+      if (!user) {
+        return next(new ErrorResponse("No user found with this id", 404));
+      }
+  
+      req.user = user;
+  
+      next();
+    } catch (error) {
+      console.log(error);
+      return next(new ErrorResponse("Not authorized to access this route, invalid token", 401));
+    }
+  };
+  
